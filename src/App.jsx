3d611e05d93acf63deb8e35c5da94d046a00b7a3
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-12-28 03:56:29 -0800

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,50 @@ import Services from "./Pages/Services";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
+import ChatIcon from "./Helpers/ChatIcon";
+import Packages from "./Pages/Packages";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      <Route
-        path="/services"
-        element={
-          <div className="pt-24 md:pt-32">
-            <Services />
-          </div>
-        }
-      />
-      <Route
-        path="/about"
-        element={
-          <div className="pt-24 md:pt-32">
-            <About />
-          </div>
-        }
-      />
-      <Route
-        path="/contact"
-        element={
-          <div className="pt-24 md:pt-32">
-            <Contact />
-          </div>
-        }
-      />
-    </Routes>
+        <Route
+          path="/services"
+          element={
+            <div className="pt-24 md:pt-32">
+              <Services />
+            </div>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <div className="pt-24 md:pt-32">
+              <About />
+            </div>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <div className="pt-24 md:pt-32">
+              <Contact />
+            </div>
+          }
+        />
+        <Route
+          path="/packages"
+          element={
+            <div className="pt-24 md:pt-32">
+              <Packages />
+            </div>
+          }
+        />
+      </Routes>
+      <ChatIcon />
+    </>
   );
 };
 
