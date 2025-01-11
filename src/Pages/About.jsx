3bd7d7480a1229d87f2a442576e2d84e4496464e
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2025-01-10 23:54:57 -0800

    linked

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -21,7 +21,10 @@ const About = () => {
       <div className="h-[60vh] bg-black flex flex-col space-y-4 items-center justify-center">
         <h1 className="text-white text-5xl font-[sans-serif]">ABOUT US</h1>
         <div className=" flex items-center text-lg font-[sans-serif] justify-center z-10">
-          <Link className=" text-white hover:text-orange-500 duration-300 ease-in-out  mr-2">
+          <Link
+            to="/"
+            className=" text-white hover:text-orange-500 duration-300 ease-in-out  mr-2"
+          >
             Home
           </Link>
           <span className=" text-orange-500  ">/ About Us</span>
